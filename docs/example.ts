82752ae4c8dfe8954cb6b6c87836305c0089877c
Ami Hollander <Ami.Hollander@microsoft.com>
2020-07-28 21:25:41 +0300

    readme

---
+++ docs/example.ts
@@ -0,0 +1,15 @@
+/*
+'JBSWY3DPEBLW64TMMQQQ====' 'SGVsbG8gV29ybGQh'
+
+"48656c6c6f20576f726c6421"
+
+"%3Fhello%3DWorld"
+*/
+export class Example {
+
+    constructor() {
+        // Comment déjà vu
+        console.log("Hello World!");
+    }
+
+}
