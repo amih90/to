ccd2230430e794312938d950b8739b2263f922d1
Ami Hollander <Ami.Hollander@microsoft.com>
2020-07-28 17:59:54 +0300

    binary format

--- src/extension.ts
+++ src/extension.ts
@@ -5,25 +5,25 @@ import { Transformer } from './transformers/transformer';
 import {
     Base32Transformer,
     Base64Transformer,
+    BinaryTransformer,
     FlatbuffersTransformer,
     HexTransformer,
     ReverseWordTransformer,
     UrlencodeTransformer,
 } from './transformers/transformers';
 
-
+// Order is important for complexity and reliability
 const transformers: Transformer[] = [
+    new BinaryTransformer(),
+    new HexTransformer(),
     new Base32Transformer(),
     new Base64Transformer(),
-    new HexTransformer(),
     new ReverseWordTransformer(),
     new UrlencodeTransformer(),
     // new FlatbuffersTransformer()
 ];
 
 export function activate(context: vscode.ExtensionContext) {
-
-
     const codelensProvider = new CodelensProvider(transformers);
     vscode.languages.registerCodeLensProvider("*", codelensProvider);
 
